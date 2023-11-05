494e9c3fa11032caf09d8da53adec9bab3e10a6d
고영성 <yeongo@goyeongseong-ui-MacBookPro.local>
2023-11-05 17:50:25 +0900

    refactor: for->while in commandQuit

--- Server_cmd.cpp
+++ Server_cmd.cpp
@@ -341,35 +341,9 @@ void	Server::commandTopic(std::vector<std::string> token, Client * user, int fd)
 
 void	Server::commandQuit(std::vector<std::string> token, Client * user, int fd) {
 	std::string msg = getTotalMessage(1, token);
-	Channel	*ch = NULL;
-	// for (std::map<std::string, Channel *>::iterator iter = _channel_list.begin();
-	// 	iter != _channel_list.end(); iter++) {
-	// 		if (ch != NULL) {
-	// 			removeChannelList(ch->getChannelName());
-	// 			deleteChannel(&ch);
-	// 		}
-	// 		ch = iter->second;
-	// 		if (ch->isChannelUser(user) == true) {
-	// 			broadcastChannelMessage(RPL_QUIT(user->getPrefix(), msg), ch, fd);
-	// 			sendMessage(RPL_PART(user->getPrefix(), ch->getChannelName()), fd);
-	// 			if(ch->isChannelOperator(user) == true)
-	// 				ch->removeChannelOperator(user);
-	// 			ch->removeChannelUser(user);
-	// 		}
-	// 		if (ch->getUserCount() == 0) {
-	// 			std::map<std::string, Channel *>::iterator tmp = iter;
-	// 			if (++tmp == _channel_list.end()) {
-	// 				removeChannelList(ch->getChannelName());
-	// 				deleteChannel(&ch);
-	// 				break ;
-	// 			}
-	// 			continue;
-	// 		}
-	// 		ch = NULL;
-	// 	}
 	std::map<std::string, Channel *>::iterator iter = _channel_list.begin();
 	while (iter != _channel_list.end()) {
-		ch = iter->second;
+		Channel *ch = iter->second;
 		++iter;
 		if (ch->isChannelUser(user) == true) {
 			broadcastChannelMessage(RPL_QUIT(user->getPrefix(), msg), ch, fd);
